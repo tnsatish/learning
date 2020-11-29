5be6e28cccb0aaacba40cf0f1fe458a6f90c22ea
tnsatish <nagasatish.tiruveedula@xome.com>
2020-11-29 09:03:47 +0530

    Added Copy files and If Files exist commands

---
+++ dotnet/Commands/CopyFiles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangedFiles
+{
+	public class CopyFiles
+	{
+		public static void Main(string[] args)
+		{
+			if(args.Length < 2)
+			{
+				Console.WriteLine("Usage: CopyFiles <InputFile> <OutputDirectory> [BasePath]\n");
+				Console.WriteLine("InputFile:       The file that contains the list of files to be copied.");
+				Console.WriteLine("OutputDirectory: Target Directory to copy all the files.");
+				Console.WriteLine("BasePath:        If BasePath is passed, it would copy only the files from this directory to the output directory.");
+				Console.WriteLine("                 It won't copy the directory strucutre of the base path in the target directory.");
+				return;
+			}
+
+			var basePath = args.Length >= 3 ? args[2] : string.Empty;
+			basePath = basePath.Replace("\\", "/");
+			var targetDir = args[1];
+			if (!targetDir.EndsWith("\\"))
+				targetDir += "\\";
+			var filePaths = File.ReadAllLines(args[0]).ToList()
+				.Where(f => f.StartsWith(basePath ?? string.Empty, StringComparison.OrdinalIgnoreCase));
+
+			foreach (var filePath in filePaths)
+			{
+				var targetFile = string.IsNullOrWhiteSpace(basePath) ? Path.Combine(targetDir, filePath) : filePath.Replace(basePath, targetDir);
+				Directory.CreateDirectory(Path.GetDirectoryName(targetFile));
+				File.Copy(filePath, targetFile, true);
+			}
+		}
+	}
+}
---
+++ dotnet/Commands/IfFilesExist.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangedFiles
+{
+	public class IfFilesExist
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length < 2)
+			{
+				Console.WriteLine("Usage: IfFilesExist <Directory> <Command> [Parameters...]\n");
+				Console.WriteLine("Directory:       Checks whether files exist in this folder or not.");
+				Console.WriteLine("Command:         Command to be executed.");
+				Console.WriteLine("Parameters:      Parameters for the command.");
+				return;
+			}
+
+			Console.WriteLine("IfFilesExist " + string.Join(" ", args));
+
+			string[] fileEntries = Directory.GetFiles(args[0]);
+			Console.WriteLine("No.of files: " + fileEntries.Length);
+			if(fileEntries.Length > 0)
+			{
+				Console.WriteLine("Files: " + Environment.NewLine + string.Join(Environment.NewLine, fileEntries));
+				List<string> parameters = args.ToList();
+				parameters.RemoveRange(0, 2);
+				Console.WriteLine(args[1]);
+				Console.WriteLine(string.Join(" ", parameters));
+
+				System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+				startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+				startInfo.RedirectStandardError = true;
+				startInfo.RedirectStandardOutput = true;
+				startInfo.FileName = args[1];
+				startInfo.UseShellExecute = false;
+				startInfo.Arguments = string.Join(" ", parameters);
+
+				System.Diagnostics.Process process = new System.Diagnostics.Process();
+				process.StartInfo = startInfo;
+				process.Start();
+				Console.WriteLine(process.StandardOutput.ReadToEnd());
+				Console.WriteLine(process.StandardError.ReadToEnd());
+			}
+		}
+	}
+}
